8946de20b85228a052666122d92f462ef6bfa545
ernestas_raudonis <raudonisernestas@gmail.com>
2020-04-03 18:23:49 +0300

    Implement navigation component

--- app/src/main/java/com/raudonikis/boardspan/MainActivity.kt
+++ app/src/main/java/com/raudonikis/boardspan/MainActivity.kt
@@ -1,12 +1,5 @@
 package com.raudonikis.boardspan
 
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
---
+++ login/src/main/java/com/raudonikis/login/ui/LoginFragment.kt
@@ -0,0 +1,6 @@
+package com.raudonikis.login.ui
+
+import androidx.fragment.app.Fragment
+import com.raudonikis.login.R
+
+class LoginFragment : Fragment(R.layout.fragment_login)
