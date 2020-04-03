8454026bbb5170ef45732daf54fce01b30f94cb0
ernestas_raudonis <raudonisernestas@gmail.com>
2020-04-03 18:08:25 +0300

    Implement better dependency management

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,20 @@
+object Versions {
+    const val navigation = "2.3.0-alpha04"
+    const val kotlin = "1.2.0"
+    const val app_compat = "1.1.0"
+    const val constraint_layout = "1.1.3"
+}
+
+object Kotlin {
+    const val kotlin_core = "androidx.core:core-ktx:${Versions.kotlin}"
+}
+
+object Support {
+    const val app_compat = "androidx.appcompat:appcompat:${Versions.app_compat}"
+    const val constraint_layout = "androidx.constraintlayout:constraintlayout:${Versions.constraint_layout}"
+}
+
+object Navigation {
+    const val navigation_fragment = "androidx.navigation:navigation-fragment-ktx:${Versions.navigation}"
+    const val navigation_ui = "androidx.navigation:navigation-ui-ktx:${Versions.navigation}"
+}
