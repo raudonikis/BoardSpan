db8333088769a38077b8150d108162bfbec4be8d
ernestas_raudonis <raudonisernestas@gmail.com>
2020-04-04 17:25:25 +0300

    Override on back pressed in login fragment

--- app/src/main/java/com/raudonikis/boardspan/MainActivity.kt
+++ app/src/main/java/com/raudonikis/boardspan/MainActivity.kt
@@ -2,11 +2,23 @@ package com.raudonikis.boardspan
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+    }
+
+    override fun onBackPressed() {
+        when (navController.currentDestination?.id) {
+            R.id.loginFragment -> finish()
+            else -> super.onBackPressed()
+        }
     }
 }
