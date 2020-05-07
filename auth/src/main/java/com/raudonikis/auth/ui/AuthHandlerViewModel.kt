a3497556896317a0f39d58957b04ac37b961f241
ernestas_raudonis <raudonisernestas@gmail.com>
2020-05-07 12:28:43 +0300

    Create a 'games' module

--- auth/src/main/java/com/raudonikis/auth/ui/AuthHandlerViewModel.kt
+++ auth/src/main/java/com/raudonikis/auth/ui/AuthHandlerViewModel.kt
@@ -6,12 +6,11 @@ import com.google.firebase.auth.FirebaseAuth
 import com.raudonikis.auth.enums.AuthState
 import javax.inject.Inject
 
-class AuthHandlerViewModel @Inject constructor(private val auth: FirebaseAuth) :
-    ViewModel() {
+class AuthHandlerViewModel @Inject constructor(private val auth: FirebaseAuth) : ViewModel() {
 
     val authenticationEvent = MutableLiveData<AuthState>()
 
-    init {
+    fun checkAuthState() {
         when {
             auth.currentUser != null -> authenticationEvent.postValue(AuthState.AUTHORIZED)
             else -> authenticationEvent.postValue(AuthState.UNAUTHORIZED)
